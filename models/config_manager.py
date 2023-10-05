a016fef5ffb5bf4e76c5691e54693f7629e0f10f
maorbe <Maor.Ben-Moshe@amdocs.com>
2023-10-05 17:11:40 +0300

    add encryption to pass in config.ini

--- models/config_manager.py
+++ models/config_manager.py
@@ -1,4 +1,7 @@
+import base64
 from configparser import ConfigParser
+
+from cryptography.fernet import Fernet
 
 from decorators.singleton_decorator import singleton
 
@@ -13,7 +16,12 @@ class ConfigManager:
         return self.config["NEXUS"]["nexus_user"], self.config["NEXUS"]["nexus_password"]
 
     def get_jenkins_cred(self):
-        return self.config["JENKINS"]["jenkins_user"], self.config["JENKINS"]["jenkins_password"]
+        f = Fernet(self.get_fernet_key())
+        return self.config["JENKINS"]["jenkins_user"], f.decrypt(self.config["JENKINS"]["jenkins_password"])
 
     def get_helm_index_url(self):
         return self.config["NEXUS"]["helm_index_url"]
+
+    def get_fernet_key(self):
+        key = self.config["DEFAULT"]["key"]
+        return base64.urlsafe_b64encode(key.encode("utf-8"))
