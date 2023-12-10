defe4d09f5a14211ffc691e248870e8a48a0473b
maorbe <Maor.Ben-Moshe@amdocs.com>
2023-12-10 14:56:46 +0200

    fix prepare response

--- app/steps/smartAnalyzeDev/smart_analyze_dev_prepare_response_step.py
+++ app/steps/smartAnalyzeDev/smart_analyze_dev_prepare_response_step.py
@@ -9,18 +9,22 @@ class PrepareResponseStep(SmartAnalyzeStepInterface):
             return
 
         groups_data = parameters.data_manager.groups_data
+        if groups_data is None or len(groups_data) == 0:
+            return
 
         total_count = sum([groups_data[key].total_flows_count for key in groups_data
                            if groups_data[key].total_flows_count > 0])
 
-        curr_flows_count = sum([groups_data[key].curr_flows_count for key in groups_data
-                                if groups_data[key].curr_flows_count > 0])
+        flows_set = set()
+        for key in groups_data:
+            if groups_data[key].curr_flows_count > 0:
+                flows_set.update(groups_data[key].flows if groups_data[key].flows else [])
 
         parameters.smart_analyze_dev_app_service_response = (SmartAnalyzeResponse.create()
                                                              .total_flows_count(total_count)
-                                                             .curr_flows_count(curr_flows_count)
+                                                             .curr_flows_count(len(flows_set))
                                                              .groups({key:
-                                                                          groups_data.get(key).serialize()
+                                                                      groups_data.get(key).serialize()
                                                                       for key in groups_data
                                                                       })
                                                              .build())
