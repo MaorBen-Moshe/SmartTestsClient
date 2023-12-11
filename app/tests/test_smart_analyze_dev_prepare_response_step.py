9c702df45c8623dad6bbb8a118d6f15bde30a246
maorbe <Maor.Ben-Moshe@amdocs.com>
2023-12-11 08:14:19 +0200

    adding tests to prepare response step of apis: smart and smart-dev

---
+++ app/tests/test_smart_analyze_dev_prepare_response_step.py
@@ -0,0 +1,44 @@
+from app.models.analyze_dev_app_params import AnalyzeDevAppServiceParameters
+from app.models.group_data import GroupData
+from app.models.service_data import ServiceData
+from app.steps.smartAnalyzeDev.smart_analyze_dev_prepare_response_step import PrepareResponseStep
+
+from app.tests.test_base import TestBase
+
+
+class TestSmartAnalyzeDevPrepareResponseStep(TestBase):
+    def setUp(self):
+        super().setUp()
+        self.step = PrepareResponseStep()
+
+    def test_execute_with_empty_groups_data(self):
+        parameters = AnalyzeDevAppServiceParameters()
+        parameters.data_manager.groups_data = {}
+        parameters.data_manager.services_map = {}
+        self.step.execute(parameters)
+        self.assertIsNone(parameters.smart_analyze_dev_app_service_response)
+
+    def test_execute_with_valid_parameters(self):
+        parameters = AnalyzeDevAppServiceParameters()
+        parameters.data_manager.groups_data = {
+            'group1': GroupData.create().total_flows_count(10).flows(['flow1', 'flow2']).build(),
+            'group2': GroupData.create().total_flows_count(20).flows(['flow3', 'flow4', 'flow5']).build(),
+            'group3': GroupData.create().total_flows_count(30).build(),
+        }
+        parameters.data_manager.services_map = {
+            'service1': ServiceData.create().flows(['flow1']).from_version("0.67.110").to_version("0.67.109").build(),
+        }
+
+        self.step.execute(parameters)
+
+        self.assertIsNotNone(parameters.smart_analyze_dev_app_service_response)
+        self.assertEqual(parameters.smart_analyze_dev_app_service_response.total_flows_count, 60)
+        self.assertEqual(parameters.smart_analyze_dev_app_service_response.curr_flows_count, 5)
+        self.assertEqual(parameters.smart_analyze_dev_app_service_response.groups, {
+            'group1': parameters.data_manager.groups_data['group1'].serialize(),
+            'group2': parameters.data_manager.groups_data['group2'].serialize(),
+            'group3': parameters.data_manager.groups_data['group3'].serialize(),
+        })
+        self.assertEqual(parameters.smart_analyze_dev_app_service_response.services, {
+            'service1': parameters.data_manager.services_map['service1'].serialize(),
+        })
